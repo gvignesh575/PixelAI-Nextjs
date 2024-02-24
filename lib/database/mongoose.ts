fd31d130bc1040fdb61029c3eae41dd0f2f12da6
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-25 00:28:27 +0530

    fixed the db issue

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGOD_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
   conn: Mongoose | null;
@@ -24,10 +24,11 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "pixelai",
+      dbName: "imaginify",
       bufferCommands: false,
     });
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
