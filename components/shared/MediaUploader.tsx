916fda359cec89abbc20e52bfdc91470738667db
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-25 15:53:38 +0530

    new codes

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,9 +1,10 @@
 "use client";
-import Image from "next/image";
-import { useToast } from "../ui/use-toast";
-import { CldImage, CldUploadWidget } from "next-cloudinary";
+
+import { useToast } from "@/components/ui/use-toast";
 import { dataUrl, getImageSize } from "@/lib/utils";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
 
 type MediaUploaderProps = {
   onValueChange: (value: string) => void;
@@ -21,27 +22,29 @@ const MediaUploader = ({
   type,
 }: MediaUploaderProps) => {
   const { toast } = useToast();
+
   const onUploadSuccessHandler = (result: any) => {
     setImage((prevState: any) => ({
       ...prevState,
       publicId: result?.info?.public_id,
       width: result?.info?.width,
       height: result?.info?.height,
-      secureUrl: result?.info?.secure_url,
+      secureURL: result?.info?.secure_url,
     }));
 
     onValueChange(result?.info?.public_id);
 
     toast({
-      title: "Image uploaded successfully!",
+      title: "Image uploaded successfully",
       description: "1 credit was deducted from your account",
       duration: 5000,
       className: "success-toast",
     });
   };
-  const onUploadErrorHandler = (result: any) => {
+
+  const onUploadErrorHandler = () => {
     toast({
-      title: "Something went wrong!",
+      title: "Something went wrong while uploading",
       description: "Please try again",
       duration: 5000,
       className: "error-toast",
@@ -49,20 +52,21 @@ const MediaUploader = ({
   };
 
   return (
-    <>
-      <CldUploadWidget
-        uploadPreset="jsm_imaginify"
-        options={{
-          multiple: false,
-          resourceType: "image",
-        }}
-        onSuccess={onUploadSuccessHandler}
-        onError={onUploadErrorHandler}
-      >
-        {({ open }) => (
-          <div className="flex flex-col gap-4">
-            <h3 className="h3-bold text-dark-600">Original</h3>
-            {publicId ? (
+    <CldUploadWidget
+      uploadPreset="jsm_imaginify"
+      options={{
+        multiple: false,
+        resourceType: "image",
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    >
+      {({ open }) => (
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">Original</h3>
+
+          {publicId ? (
+            <>
               <div className="cursor-pointer overflow-hidden rounded-[10px]">
                 <CldImage
                   width={getImageSize(type, image, "width")}
@@ -74,23 +78,23 @@ const MediaUploader = ({
                   className="media-uploader_cldImage"
                 />
               </div>
-            ) : (
-              <div className="media-uploader_cta" onClick={() => open()}>
-                <div className="media-uploader_cta-image">
-                  <Image
-                    src="/assets/icons/add.svg"
-                    alt="Add Image"
-                    width={24}
-                    height={24}
-                  />
-                </div>
-                <p className="p-14-medium">Click here to upload image</p>
+            </>
+          ) : (
+            <div className="media-uploader_cta" onClick={() => open()}>
+              <div className="media-uploader_cta-image">
+                <Image
+                  src="/assets/icons/add.svg"
+                  alt="Add Image"
+                  width={24}
+                  height={24}
+                />
               </div>
-            )}
-          </div>
-        )}
-      </CldUploadWidget>
-    </>
+              <p className="p-14-medium">Click here to upload image</p>
+            </div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
   );
 };
 
