a25a1df3f9e719a8647f386a73d5317e38363894
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-25 16:07:19 +0530

    fixed the upload preset error

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="jsm_pixelai"
       options={{
         multiple: false,
         resourceType: "image",
