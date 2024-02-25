28f2257cf79a2739f456156458fc5307fe13b009
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-25 15:50:08 +0530

    new code

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,8 @@
 import Header from "@/components/shared/Header";
-import React from "react";
-import { transformationTypes } from "@/constants";
 import TransformationForm from "@/components/shared/TransformationForm";
-import { auth } from "@clerk/nextjs";
+import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const AddTransformationTypePage = async ({
@@ -11,12 +10,15 @@ const AddTransformationTypePage = async ({
 }: SearchParamProps) => {
   const { userId } = auth();
   const transformation = transformationTypes[type];
+
   if (!userId) redirect("/sign-in");
+
   const user = await getUserById(userId);
 
   return (
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
+
       <section className="mt-10">
         <TransformationForm
           action="Add"
