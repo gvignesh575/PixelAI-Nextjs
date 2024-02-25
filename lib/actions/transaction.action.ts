79aaf090b3dde35e1f34f3c2d0c78638ec2c7c07
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-26 00:00:41 +0530

    implemented the stripe payments

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -42,12 +42,14 @@ export async function createTransaction(transaction: CreateTransactionParams) {
   try {
     await connectToDatabase();
 
+    // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
       ...transaction,
       buyer: transaction.buyerId,
     });
 
     await updateCredits(transaction.buyerId, transaction.credits);
+
     return JSON.parse(JSON.stringify(newTransaction));
   } catch (error) {
     handleError(error);
